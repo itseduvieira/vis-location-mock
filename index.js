0b2c01d896be7544bdd96cbbfc8aa2df7b08bf90
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-08-26 20:09:44 -0300

    Commit

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const admin = require('firebase-admin')
+const serviceAccount = require('./serviceAccountKey.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://visual-no-ar.firebaseio.com/'
+})
+
+let campaign = '5b79e392159119117d9d0fdb'
+
+let coordinates = {
+    'latitude': -54.399748,
+    'longitude': -69.732339,
+    'altitude': 100,
+    'description': 'City, State'
+}
+
+let db = admin.database()
+let cref = db.ref('/campaign').child(campaign)
+let location = cref.child('location')
+let active = cref.child('active')
+
+active.set(true)
+
+function toFixed (num, fixed) {
+    fixed = fixed || 0;
+    fixed = Math.pow(10, fixed);
+    return Math.floor(num * fixed) / fixed;
+}
+
+function waitAndDo(times) {
+    if(times < 1) {
+        active.set(false)
+
+        admin.app().delete()
+
+        return
+    }
+  
+    setTimeout(function() {
+        location.set(coordinates)
+
+        console.log(`set ${times} ${JSON.stringify(coordinates)}`)
+
+        let latitude = toFixed((coordinates.latitude + 0.2), 6)
+
+        if(latitude > 89.9) {
+            latitude = -89.9
+        }
+
+        coordinates.latitude = latitude
+
+        let longitude = toFixed((coordinates.longitude + (0.04 + (times * 0.001))), 6)
+
+        if(longitude > 179.9) {
+            longitude = -179.9
+        }
+
+        coordinates.longitude = longitude
+  
+        waitAndDo(times - 1)
+    }, 2500)
+}
+
+waitAndDo(50)
