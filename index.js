6ee09588a19564e533e466fba38250095d8b99fe
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-11-23 03:23:49 -0200

    Circle path coordinates

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ admin.initializeApp({
     databaseURL: 'https://visual-no-ar.firebaseio.com/'
 })
 
-let campaign = '5b8ec78c581ed000142af107'
+let campaign = '5bc1432b3cf2670013274a22'
+// let campaign = '5bbebbda9d8c2d8171fc3194'
+
+let start = new Date().getTime()
 
 let coordinates = {
-    'latitude': -54.399748,
-    'longitude': -69.732339,
+    'latitude': 0,
+    'longitude': 0,
     'altitude': 100,
     'description': 'City, State'
 }
@@ -43,27 +46,38 @@ function waitAndDo(times) {
 
             console.log(`set ${times} ${JSON.stringify(coordinates)}`)
 
-            let latitude = toFixed((coordinates.latitude + 0.2), 6)
+            // let latitude = toFixed((coordinates.latitude + 0.2), 6)
 
-            if(latitude > 89.9) {
-                latitude = -89.9
-            }
+            // if(latitude > 89.9) {
+            //     latitude = -89.9
+            // }
 
-            coordinates.latitude = latitude
+            // coordinates.latitude = latitude
 
-            let longitude = toFixed((coordinates.longitude + 0.04), 6)
+            // let longitude = toFixed((coordinates.longitude + 0.04), 6)
 
-            if(longitude > 179.9) {
-                longitude = -179.9
-            }
+            // if(longitude > 179.9) {
+            //     longitude = -179.9
+            // }
 
-            coordinates.longitude = longitude
+            // coordinates.longitude = longitude
+
+            // let now = new Date().getTime() - start
+
+            // coordinates.longitude = Math.cos(now) * 20
+            // coordinates.latitude = Math.sin(now) * 20
+
+            // coordinates.longitude = toFixed(20 * Math.cos(2 * Math.PI * times / 20), 6)
+            // coordinates.latitude = toFixed(20 * Math.sin(2 * Math.PI * times / 20), 6)
+
+            coordinates.longitude = toFixed(20 * Math.cos(2 * Math.PI * times / 100), 6)
+            coordinates.latitude = toFixed(20 * Math.sin(2 * Math.PI * times / 100), 6)
 
             coordinates.description = (times % 5 === 0) ? 'City, State 1' : 'Jonas, State 2'
     
             waitAndDo(times - 1)
-        }, 2500)
+        }, 1000)
     }
 }
 
-waitAndDo(10)
+waitAndDo(1000)
